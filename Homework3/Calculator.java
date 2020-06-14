edcb82ab1e43ff8b077da0aa65d4e688042ccf9f
PliakhnevichAliaksandr <Plahnevich.Alexander@pgo.local>
2020-06-14 16:18:52 +0300

    Modified files of Homework3 after correction

--- Homework3/Calculator.java
+++ Homework3/Calculator.java
@@ -1,4 +1,6 @@
-package JC1.Homework.Homework3.Homework3v1;
+package JC1.Homework.Homework3.Homework3_Correction1;
+
+import static JC1.Homework.Homework3.Homework3_Correction1.Task1_Homework3.userChoiseMenu;
 
 public class Calculator {
 
@@ -7,32 +9,32 @@ public class Calculator {
     
     Calculator(double firstNum, double secondNum){
     this.firstNum = firstNum;
-    this.secondNum = secondNum;
-    }    
+    this.secondNum = secondNum;}    
 
-    void mathActions(int userChoise){
+    double mathActions(int userChoise){
+        // исправил
+        
+        /** NOTE! В этом методе лучше просто производить вычисления и
+         * возвращать результат, а выводить сообщение лучше во втором
+         * классе потому, что именно он тебя работает с консолью.
+         * Данный класс предназначен только для выполнения арифметических операций.
+         * Он не должен уметь печатать текст в консоль. Есть такой "Принцип единсственной
+         * ответственности" он как раз говорит что класс должен делать именно то, для чего он
+         * предназначен и ничего больше. Соответстенно Calculator должен только считать.*/
         // арифметические операции калькулятора
         switch(userChoise){
-            case 1: {
-                double res = firstNum + secondNum;
-                System.out.println("Результат равен: " + res + "\n");
-                break;
-            }
-            case 2: {
-                double res = firstNum - secondNum;
-                System.out.println("Результат равен: " + res + "\n");
-                break;
-            }
-            case 3: {
-                double res = firstNum * secondNum;
-                System.out.println("Результат равен: " + res + "\n");
-                break;
-            }
-            case 4: {
-                double res = firstNum / secondNum;
-                System.out.println("Результат равен: " + res + "\n");
-                break;
-            }
+            case 1: 
+                return firstNum + secondNum;
+            case 2: 
+                return firstNum - secondNum;
+            case 3:
+                return firstNum * secondNum;
+            case 4:
+                return firstNum / secondNum;
+            // требует return хоть у меня на вход метода приходит только проверенный 
+            // параметр userChoise = 1...4, просто пишу default: return 0.0;
+            // но как правильно сделать в этом случае?    
+            default: return 0.0;    
         }
     }   
 }
--- Homework3/Task1_Homework3.java
+++ Homework3/Task1_Homework3.java
@@ -1,105 +1,105 @@
-package JC1.Homework.Homework3.Homework3v1;
+package JC1.Homework.Homework3.Homework3_Correction1;
 
 import java.util.Scanner;
 
 public class Task1_Homework3 {
-    
+
     static int userChoiseMenu;
     static int ifDivide0;
-    
-    static void calculatorMenu(){
+
+    static void calculatorMenu() {
         // вывод меню и запуск метода scanMenu() с привоением значения полю userChoiseMenu
-            System.out.println("Выберите функцию");
-            System.out.println("1: Сложение");
-            System.out.println("2: Разность");
-            System.out.println("3: Умножение");
-            System.out.println("4: Деление");
-            System.out.println("0: Выход");
-            userChoiseMenu = scanMenu();
-        } 
-    static int scanMenu() {
-        // главное меню, возвращает 0, 1, 2 ,3, 4
-        while (true){
-            Scanner scan = new Scanner(System.in);
-            if (scan.hasNextInt()){
-                int userChoiseMenu = scan.nextInt();
-                if (userChoiseMenu >= 0 && userChoiseMenu <= 4){
-                    return userChoiseMenu;
-                } else {
-                    System.out.println("Введите корректное значение");
-                }                
-            } else {
-                System.out.println("Введите корректное значение");
-            }            
-        }        
+        System.out.println("Выберите функцию");
+        System.out.println("1: Сложение");
+        System.out.println("2: Разность");
+        System.out.println("3: Умножение");
+        System.out.println("4: Деление");
+        System.out.println("0: Выход");
+        userChoiseMenu = scanMenu();
     }
-    static int scanMenuDivide0(){
+
+    static int scanMenu() {
+    // главное меню, возвращает 0, 1, 2 ,3, 4
+    // убрал блоки else и убрал цикл while, последний return запускает метод заново
+    /** NOTE! Тут можно немного упростить код:
+     * Убрать else блоки, а вывод сообщения деать в конце тела цикла.
+     * Ты же все равно заканчиваешь этот цикл, только если число от 0 до 4
+     * в любой другой ситуации ты выводишь сообщение об ошибке.*/
+        Scanner scan = new Scanner(System.in);
+        if (scan.hasNextInt()) {
+            int userChoiseMenu = scan.nextInt();
+            if (userChoiseMenu >= 0 && userChoiseMenu <= 4) {
+                return userChoiseMenu;
+            }
+        }    
+        System.out.println("Введите корректное значение");
+        return scanMenu();
+    }
+
+    static int scanMenuDivide0() {
         // меню при делении на 0, возвращает 1 или 2
-        while (true){
-            Scanner scan = new Scanner(System.in);
-            if (scan.hasNextInt()){
-                int ifDivide0 = scan.nextInt();
-                if (ifDivide0 >= 1 && ifDivide0 <= 2){
-                    return ifDivide0;
-                } else {
-                    System.out.println("Введите корректное значение");
-                }                
-            } else {
-                System.out.println("Введите корректное значение");
-            }            
-        }        
-    }        
-    static double getFirstNumber(){
-        // ввод и возвращение первого числа с проверкой
-        System.out.println("Введите первое число:");
-        while (true){
-            Scanner scan = new Scanner(System.in);
-            if (scan.hasNextDouble()){
-                double firstNum = scan.nextDouble();
-                return firstNum;               
-            } else {
-                System.out.println("Введите корректное значение");
+        // убрал блоки else и убрал цикл while, последний return запускает метод заново
+        Scanner scan = new Scanner(System.in);
+        /** NOTE! Тут можно немного упростить код:
+         * Убрать else блоки, а вывод сообщения деать в конце тела цикла.
+         * Ты же все равно заканчиваешь этот цикл, только если число от 0 до 4
+         * в любой другой ситуации ты выводишь сообщение об ошибке.*/
+        if (scan.hasNextInt()) {
+            int ifDivide0 = scan.nextInt();
+            if (ifDivide0 >= 1 && ifDivide0 <= 2) {
+                return ifDivide0;
+            } 
+        }
+        System.out.println("Введите корректное значение");
+        return scanMenuDivide0();    
+    }
+
+    static double getNumber(String s) {
+        // ввод и возвращение числа с проверкой
+        System.out.println(s);
+        Scanner scan = new Scanner(System.in);
+        // устранил
+        /** NOTE! Тут можно немного упростить код:
+         * Убрать else блоки, а вывод сообщения деать в конце тела цикла.
+         * Ты же все равно заканчиваешь этот цикл, только если число от 0 до 4
+         * в любой другой ситуации ты выводишь сообщение об ошибке.*/
+        if (scan.hasNextDouble()) {
+            /** NOTE! scan.nextDouble можно написать сразу рядом с ретурном.
+             * Нет смысла заводить отдельную переменную.*/
+            // устранил
+            return scan.nextDouble();
+        }
+        System.out.println("Введите корректное значение");
+        return getNumber(s);    
+    }
+    // делаю общий метод getNumber();
+    /** NOTE! getFirstNumber и getSecondNumber по сути делают одно и то же.
+     * По сути можно написать общую функцию, а в качестве парметра к этой функции
+     * можно передавать отображаемые сообщения*/
+
+    static void goCalc() {
+        // создание нового объекта класса Calculator, проверка при делении на 0
+        Calculator calc = new Calculator(getNumber("Введите первое число"), getNumber("Введите второе число"));
+        if (userChoiseMenu == 4 && calc.secondNum == 0) {
+            System.out.println("Делить на 0 нельзя");
+            System.out.println("1: Ввести другое число");
+            System.out.println("2: Выйти в меню");
+            ifDivide0 = scanMenuDivide0();
+            switch (ifDivide0) {
+                case 1: 
+                    // запрашиваем новое первое число
+                    calc.firstNum = getNumber("Введите первое число");
+                    // запрашиваем новое второе число, не равное 0
+                    do {
+                        calc.secondNum = getNumber("Введите второе число");
+                    }
+                    while (calc.secondNum == 0);
+                    break;
+                case 2:
+                    return;
             }
         }
-    }    
-    static double getSecondNumber(){
-        // ввод и возвращение второго числа с проверкой
-        System.out.println("Введите второе число:");
-        while (true){
-            Scanner scan = new Scanner(System.in);
-            if (scan.hasNextDouble()){
-                double secondNum = scan.nextDouble();     
-                return secondNum;
-            } else {
-                System.out.println("Введите корректное значение");
-            }
-        }        
-    }
-    static void goCalc(){
-        // создание нового объекта класса Calculator, проверка при делении на 0
-        Calculator calc = new Calculator(getFirstNumber(), getSecondNumber());
-            if (userChoiseMenu == 4 && calc.secondNum == 0){
-                System.out.println("Делить на 0 нельзя");
-                System.out.println("1: Ввести другое число");
-                System.out.println("2: Выйти в меню");
-                ifDivide0 = scanMenuDivide0();
-                switch (ifDivide0){
-                    case 1: {
-                        // запрашиваем новое первое число
-                        calc.firstNum = getFirstNumber();
-                        // запрашиваем новое второе число, не равное 0
-                        do {
-                            calc.secondNum = getSecondNumber();
-                        }
-                        while (calc.secondNum == 0);
-                        break;
-                    }
-                    case 2: {
-                        return;
-                    }                    
-                }
-            }
-        calc.mathActions(userChoiseMenu);
+        System.out.println("Результат равен: " + calc.mathActions(userChoiseMenu) + "\n");    
     }
 
     public static void main(String[] args) {
@@ -110,8 +110,8 @@ public class Task1_Homework3 {
                 goCalc();
             } else {
                 break;
-            }            
+            }
         }
         while (true);
-    }    
+    }
 }
